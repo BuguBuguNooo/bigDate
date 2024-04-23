d16788ca39360499b59b8760d46ad1709fd910f2
Jay Ge <32101073@stu.hzcu.edu.cn>
2024-04-23 14:36:33 +0800

    哈哈

--- src/test/java/com/gfj/yellodate/Demo.java
+++ src/test/java/com/gfj/yellodate/Demo.java
@@ -1,61 +1,61 @@
 package com.gfj.yellodate;
-
-import com.aliyun.oss.ClientException;
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.common.auth.*;
-import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.OSSException;
-import com.aliyun.oss.model.PutObjectRequest;
-import com.aliyun.oss.model.PutObjectResult;
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-
+//
+//import com.aliyun.oss.ClientException;
+//import com.aliyun.oss.OSS;
+//import com.aliyun.oss.common.auth.*;
+//import com.aliyun.oss.OSSClientBuilder;
+//import com.aliyun.oss.OSSException;
+//import com.aliyun.oss.model.PutObjectRequest;
+//import com.aliyun.oss.model.PutObjectResult;
+//import java.io.ByteArrayInputStream;
+//import java.io.FileInputStream;
+//
 public class Demo {
-
-    public static void main(String[] args) throws Exception {
-        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
-        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-        //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-        String AccessKeyId = 
-        String AccessKeySecret = 
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = 
-        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        String objectName = 
-
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint,AccessKeyId,AccessKeySecret);
-
-        try {
-
-            // 创建PutObjectRequest对象。
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new FileInputStream("/Users/gefangjie/Desktop/Pic/全平台头像.jpg"));
-
-            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
-            // ObjectMetadata metadata = new ObjectMetadata();
-            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
-            // metadata.setObjectAcl(CannedAccessControlList.Private);
-            // putObjectRequest.setMetadata(metadata);
-
-            // 上传字符串。
-            PutObjectResult result = ossClient.putObject(putObjectRequest);
-        } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-                    + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
-        } catch (ClientException ce) {
-            System.out.println("Caught an ClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with OSS, "
-                    + "such as not being able to access the network.");
-            System.out.println("Error Message:" + ce.getMessage());
-        } finally {
-            if (ossClient != null) {
-                ossClient.shutdown();
-            }
-        }
-    }
+//
+//    public static void main(String[] args) throws Exception {
+//        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+//        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+//        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+//        //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+//        String AccessKeyId =
+////        String AccessKeySecret =
+////        // 填写Bucket名称，例如examplebucket。
+////        String bucketName =
+////        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+////        String objectName =
+////
+////        // 创建OSSClient实例。
+////        OSS ossClient = new OSSClientBuilder().build(endpoint,AccessKeyId,AccessKeySecret);
+//
+//        try {
+//
+////            // 创建PutObjectRequest对象。
+////            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new FileInputStream("/Users/gefangjie/Desktop/Pic/全平台头像.jpg"));
+////
+////            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+////            // ObjectMetadata metadata = new ObjectMetadata();
+////            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+////            // metadata.setObjectAcl(CannedAccessControlList.Private);
+////            // putObjectRequest.setMetadata(metadata);
+////
+////            // 上传字符串。
+////            PutObjectResult result = ossClient.putObject(putObjectRequest);
+//        } catch (OSSException oe) {
+//            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+//                    + "but was rejected with an error response for some reason.");
+//            System.out.println("Error Message:" + oe.getErrorMessage());
+//            System.out.println("Error Code:" + oe.getErrorCode());
+//            System.out.println("Request ID:" + oe.getRequestId());
+//            System.out.println("Host ID:" + oe.getHostId());
+////        } catch (ClientException ce) {
+////            System.out.println("Caught an ClientException, which means the client encountered "
+////                    + "a serious internal problem while trying to communicate with OSS, "
+////                    + "such as not being able to access the network.");
+////            System.out.println("Error Message:" + ce.getMessage());
+//////        } finally {
+//////            if (ossClient != null) {
+//////                ossClient.shutdown();
+//////            }
+//////        }
+//////    }
 }
